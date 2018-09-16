dfb31746cf3717d27a3862703444570624b139e0
Toan Nguyen <n.minhtoan@gmail.com>
2018-09-16 00:26:11 -0700

    add login form & register user on firebase

--- app/src/main/java/com/project/groupproject/MainActivity.java
+++ app/src/main/java/com/project/groupproject/MainActivity.java
@@ -1,23 +1,87 @@
 package com.project.groupproject;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth mAuth;
+    private static String TAG = "login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // firebase
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        // init fỉebase auth instance
+        mAuth = FirebaseAuth.getInstance();
 
-        myRef.setValue("Hello, World!");
+        // init edit text
+        inputEmail = findViewById(R.id.input_email);
+        inputPassword = findViewById(R.id.input_password);
 
+
+        // init button and assign event listener
+        mButton = findViewById(R.id.button_login);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+    }
+
+
+    private String getUsername(){
+        return inputEmail.getText().toString();
+    }
+
+    private String getPassword(){
+        return inputPassword.getText().toString();
+    }
+
+    private void register() {
+        String email = getUsername();
+        String password = getPassword();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Register successfully.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+                        }
+
+                    }
+                });
     }
 }
