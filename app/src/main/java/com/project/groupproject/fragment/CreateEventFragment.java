940d7dec3d43f7d1c3dc9efb671cccce7aa9de8f
Scarlett <scarlett.truong307@gmail.com>
2018-11-14 21:20:04 -0800

    Add toolbars user profile &create event

--- app/src/main/java/com/project/groupproject/fragment/CreateEventFragment.java
+++ app/src/main/java/com/project/groupproject/fragment/CreateEventFragment.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
